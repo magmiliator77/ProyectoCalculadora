0b1ee55070d7e426134527c49458c41a1fed435b
MiguelTorresMartínez <veinte2007mtm23@gmail.com>
2026-03-02 18:00:49 +0100

    Agrega clase pruebaCalculadora

---
+++ pruebaCalculadora.java
@@ -0,0 +1,7 @@
+public class pruebaCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora(10, 3);
+        System.out.println("Suma: " + calc.suma());
+    }
+}
